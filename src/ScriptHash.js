26cfaadead0bf4687722807526ef28c3e782a04a
Christian Schmitz <cschmitz398@gmail.com>
2024-03-19 10:56:42 -0600

    exported ScriptHash

--- src/ScriptHash.js
+++ src/ScriptHash.js
@@ -1,11 +1,12 @@
 /**
  * @typedef {import("@helios-lang/codec-utils").ByteArrayLike} ByteArrayLike
+ * @typedef {import("@helios-lang/uplc").UplcData} UplcData
  * @typedef {import("./Hash.js").Hash} Hash
  */
 
-import { encodeBytes } from "@helios-lang/cbor"
+import { decodeBytes, encodeBytes } from "@helios-lang/cbor"
 import { bytesToHex, toBytes } from "@helios-lang/codec-utils"
-import { ByteArrayData } from "@helios-lang/uplc"
+import { ByteArrayData, decodeUplcData } from "@helios-lang/uplc"
 
 /**
  * @implements {Hash}
@@ -23,6 +24,38 @@ export class ScriptHash {
      */
     constructor(bytes) {
         this.bytes = toBytes(bytes)
+    }
+
+    /**
+     * @param {ScriptHash | ByteArrayLike} arg
+     * @returns {ScriptHash}
+     */
+    static from(arg) {
+        return arg instanceof ScriptHash ? arg : new ScriptHash(arg)
+    }
+
+    /**
+     * @param {number[]} bytes
+     * @returns {ScriptHash}
+     */
+    static fromCbor(bytes) {
+        return new ScriptHash(decodeBytes(bytes))
+    }
+
+    /**
+     * @param {UplcData} data
+     * @returns {ScriptHash}
+     */
+    static fromUplcData(data) {
+        return new ScriptHash(ByteArrayData.expect(data).bytes)
+    }
+
+    /**
+     * @param {string | number[]} bytes
+     * @returns {ScriptHash}
+     */
+    static fromUplcCbor(bytes) {
+        return ScriptHash.fromUplcData(decodeUplcData(bytes))
     }
 
     /**
