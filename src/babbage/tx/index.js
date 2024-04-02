1ee44482f80cdd172dfc9e89e5619b45ba70385b
Christian Schmitz <cschmitz398@gmail.com>
2024-04-01 21:02:43 -0600

    TxBuilder exported

--- src/babbage/tx/index.js
+++ src/babbage/tx/index.js
@@ -2,11 +2,16 @@ export { Address } from "./Address.js"
 export { PaymentCredential as Credential } from "./PaymentCredential.js"
 export { DCert } from "./DCert.js"
 export { StakingCredential } from "./StakingCredential.js"
+export { Tx } from "./Tx.js"
+export { TxBody } from "./TxBody.js"
+export { TxBuilder } from "./TxBuilder.js"
 export { TxId } from "./TxId.js"
 export { TxInput } from "./TxInput.js"
 export { TxOutput } from "./TxOutput.js"
 export { TxOutputDatum } from "./TxOutputDatum.js"
 export { TxOutputId } from "./TxOutputId.js"
+export { TxRedeemer } from "./TxRedeemer.js"
+export { TxWitnesses } from "./TxWitnesses.js"
 
 /**
  * @typedef {import("./TxOutputDatum.js").TxOutputDatumKind} TxOutputDatumKind
